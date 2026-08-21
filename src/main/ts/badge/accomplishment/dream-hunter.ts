6cce853259f3669e5c82c5167ae0058dcc1de169
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2026-08-21 15:32:29 -0700

    Update dream-hunter.ts

--- src/main/ts/badge/accomplishment/dream-hunter.ts
+++ src/main/ts/badge/accomplishment/dream-hunter.ts
@@ -7,7 +7,7 @@ export const DreamHunter: BadgeData = {
   setTitleId: [2605],
   name: 'Dream Hunter',
   releaseDate: '2026-12-30',
-  morality: 'any,
+  morality: 'all',
   badgeText: `You've had plenty of practice stalking the frightful nightmares in the Labyrinth of Fog, working together with the forces of Achlys' Four Generals to hold the Origin of Apotheosis. Flipping the tables on the Labyrinthine Nightmare's power dichotomy and helping them understand what they should really be afraid of.`,
   acquisition: `Complete The Hunter, Glykeria's nightmare hunt mission offered in the Origin of Apotheosis of the Labyrinth of Fog`,
   links: [
