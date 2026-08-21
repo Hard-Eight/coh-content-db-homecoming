9f1de54e771ae618a5989ce20f45ecece7350089
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2026-08-21 15:58:25 -0700

    Update dream-hunter.ts

--- src/main/ts/badge/accomplishment/dream-hunter.ts
+++ src/main/ts/badge/accomplishment/dream-hunter.ts
@@ -1,4 +1,4 @@
-import { BadgeData, missionLink, missionUri } from 'coh-content-db'
+import { BadgeData } from 'coh-content-db'
 
 export const DreamHunter: BadgeData = {
   type: 'accomplishment',
@@ -9,7 +9,7 @@ export const DreamHunter: BadgeData = {
   releaseDate: '2026-12-30',
   morality: 'all',
   badgeText: `You've had plenty of practice stalking the frightful nightmares in the Labyrinth of Fog, working together with the forces of Achlys' Four Generals to hold the Origin of Apotheosis. Flipping the tables on the Labyrinthine Nightmare's power dichotomy and helping them understand what they should really be afraid of.`,
-  acquisition: `Complete The Hunter, Glykeria's nightmare hunt mission offered in the Origin of Apotheosis of the Labyrinth of Fog`,
+  acquisition: `Complete The Hunter, Glykeria's nightmare hunt mission offered in the Origin of Apotheosis of the Labyrinth of Fog.`,
   links: [
     { title: 'Dream Hunter Badge', href: 'https://homecoming.wiki/wiki/Dream_Hunter_Badge' },
   ],
