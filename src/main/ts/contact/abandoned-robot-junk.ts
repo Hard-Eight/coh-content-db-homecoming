575f8b56ac3fe030dc42a9e6ed9ad115a8895f3d
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2026-08-21 14:45:42 -0700

    Update abandoned-robot-junk.ts

--- src/main/ts/contact/abandoned-robot-junk.ts
+++ src/main/ts/contact/abandoned-robot-junk.ts
@@ -6,7 +6,7 @@ export const AbandonedRobotJunk: ContactData = {
   name: 'Abandoned Robot Junk',
   title: 'Bizarre talking Robot',
   morality: 'villainous',
-  location: { zoneKey: KallistiWharf.key, coords: [554 -245 5064] },
+  location: { zoneKey: KallistiWharf.key, coords: [554, -245, 5064] },
   levelRange: [40, 50],
   links: [{ title: 'Abandoned Robot Junk', href: 'https://homecoming.wiki/wiki/Abandoned_Robot_Junk' }],
 }
