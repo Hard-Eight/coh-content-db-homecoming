67b13b0829e03859d5d9c5a0a606f6ca417be4cc
Hard-Eight <52937758+Hard-Eight@users.noreply.github.com>
2026-08-21 00:54:32 -0700

    Removed period in badge name.

--- src/main/ts/badge/accomplishment/you-should-see-the-other-guy.ts
+++ src/main/ts/badge/accomplishment/you-should-see-the-other-guy.ts
@@ -6,12 +6,12 @@ export const YouShouldSeeTheOtherGuy: BadgeData = {
   key: 'you-should-see-the-other-guy',
   gameId: 'Mission_KR_i24_Personal_Story',
   setTitleId: [2317],
-  name: 'You should see the other guy.',
+  name: 'You should see the other guy',
   releaseDate: '2012-11-30',
   morality: 'heroic',
   badgeText: `You played Deadlock's personal story and learned a little bit about the man who put Back Alley Brawler in the hospital.`,
   links: [
-    { title: 'You should see the other guy. Badge', href: 'https://homecoming.wiki/wiki/You_should_see_the_other_guy_Badge' },
+    { title: 'You should see the other guy Badge', href: 'https://homecoming.wiki/wiki/You_should_see_the_other_guy_Badge' },
   ],
   icon: 'https://n15g.github.io/coh-content-db-homecoming/images/badges/accomplishment/st-martial-parc.png',
   requirements: [
